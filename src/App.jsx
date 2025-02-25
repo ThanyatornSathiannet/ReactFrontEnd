d68add00116ed815185f77678b4bc7e242a5691e
ThanyatornSathiannet <s6606021610079@email.kmutnb.ac.th>
2025-02-25 11:03:56 +0700

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Hello from './assets/Hello';
+import Contact from './assets/Contact';
+import Counter from './assets/Counter';
 
 function App() {
-  return (
-    <div className='App'>
-      <Hello />
-    </div>
-  );
+    const helloData = [
+      {name: 'Thanyatorn', message: 'Hi There'},
+      {name: 'Nhoon', message: 'Hello...'}
+    ];
+
+    return (
+      <div>
+        <Counter />
+        {helloData.map((data, index) => (
+          <Hello key={index} name={data.name} message={data.message} />
+        ))}
+        <Contact email="s6606021610079@gmail.com" phone="0612246941" />
+      </div>
+    );
 }
 
 export default App;
